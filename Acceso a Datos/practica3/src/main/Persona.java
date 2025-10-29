11fa3650ed03affe64c382fe9670f4b03db3d114
Qaffqa <183117977+vjp-danielMD@users.noreply.github.com>
2025-10-29 13:59:47 +0100

    qweqwe

---
+++ Acceso a Datos/practica3/src/main/Persona.java
@@ -0,0 +1,59 @@
+package main;
+
+import java.io.Serializable;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/**
+ *
+ * @author danie
+ */
+public class Persona implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private String nombre;
+    private int edad;
+    private String ciudad;
+
+    public Persona(String nombre, int edad, String ciudad) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.ciudad = ciudad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona {\n"
+                + "    Nombre: " + nombre + "\n"
+                + "    Edad  : " + edad + "\n"
+                + "    Ciudad: " + ciudad + "\n"
+                + "}";
+    }
+
+}
