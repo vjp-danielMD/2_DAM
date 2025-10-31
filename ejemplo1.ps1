32c84a62bdda4c7f536fcc4bb3e04903747412ef
Daniel M. <dmorenod10@informatica.iesvalledeljerteplasencia.es>
2025-10-31 17:55:04 +0100

    Create ejemplo1.ps1

---
+++ ejemplo1.ps1
@@ -0,0 +1,7 @@
+[string]$Greetings = "Hello Wolrd!"
+[int]$Count = 10
+[bool]$IsEnabled = $true
+
+Write-Host "Greeting: " $Greetings
+Write-Host "Count: " $Count
+Write-Host "Is Enabled: " $IsEnabled
