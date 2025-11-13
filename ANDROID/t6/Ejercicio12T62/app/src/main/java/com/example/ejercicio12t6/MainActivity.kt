ff97d66766a42dcc92af2fb1604768a67a3c6f88
Qaffqa <183117977+vjp-danielMD@users.noreply.github.com>
2025-11-13 10:47:45 +0100

    sdasd

--- ANDROID/t6/Ejercicio12T62/app/src/main/java/com/example/ejercicio12t6/MainActivity.kt
+++ ANDROID/t6/Ejercicio12T62/app/src/main/java/com/example/ejercicio12t6/MainActivity.kt
@@ -1,20 +1,56 @@
 package com.example.ejercicio12t6
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val grupoPrimera: RadioGroup = findViewById<RadioGroup>(R.id.grupoPrimera)
+        val grupoSegunda: RadioGroup = findViewById<RadioGroup>(R.id.grupoSegunda)
+        val grupoTercera: RadioGroup = findViewById<RadioGroup>(R.id.grupoTercera)
+        val botonEnviar: Button = findViewById<Button>(R.id.buttonSend)
+
+        botonEnviar.setOnClickListener {
+            var aciertos = 0;
+            val total = 3;
+
+            //  respuestas correctas
+            val idPrimera = grupoPrimera.checkedRadioButtonId
+            val idSegunda = grupoSegunda.checkedRadioButtonId
+            val idTercera = grupoTercera.checkedRadioButtonId
+
+            if (idPrimera != -1) {
+                val seleccion: RadioButton = findViewById<RadioButton>(idPrimera)
+                if (seleccion.text == getString(R.string.primera_op_uno)) aciertos++
+            }
+
+            if (idSegunda != -1) {
+                val seleccion = findViewById<RadioButton>(idSegunda)
+                if (seleccion.text == getString(R.string.segunda_op_uno)) aciertos++
+            }
+
+            if (idTercera != -1) {
+                val seleccion = findViewById<RadioButton>(idTercera)
+                if (seleccion.text == getString(R.string.tercera_op_tres)) aciertos++
+            }
+
+            val porcentaje = (aciertos.toDouble() / total * 100).toInt()
+
+            val mensaje = getString(R.string.toast, porcentaje)
+
+            Toast.makeText(this, mensaje, Toast.LENGTH_LONG).show()
+
         }
     }
 }
+
